617d00b17ff6666198bca91f7e0f64ec057c9fb7
marvinramirez0202-coder <marvinramirez0202@gmail.com>
2026-03-17 17:37:08 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,23 @@ int main()
     cin >> num1 >> num2 >> num3;
 
     // TODO: Find the smallest value among three values
+    if(num1 < num2){
+        if(num1 < num3){
+            min = num1;
+        }
+        else{
+            min = num3;
+        }
+    }
+    else{
+        if(num2 < num3){
+            min = num2;
+        }
+        else{
+      min = num3;
+        }
+    }   
 
     // Use the following statements to print output
-    cout << "The smallest number is  " << min << endl;
+    cout << "The smallest number is " << min << endl;
 }
